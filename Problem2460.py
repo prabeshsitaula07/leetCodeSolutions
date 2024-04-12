77018cf6a92d2962a15ab9478d842e2edf1ab3ba
Prabesh Sitaula <sitaulaprabesh07@gmail.com>
2024-04-12 19:09:24 +0545

    Apply Operations to an Array

---
+++ Problem2460.py
@@ -0,0 +1,14 @@
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        zeros=0
+        i=0
+        while(i<(len(nums)-1)):
+            if(nums[i]==nums[i+1]):
+                nums[i]*=2
+                nums[i+1]=0
+                i+=1
+            i+=1
+            # print(nums)
+        zeros=nums.count(0)
+        nums = [i for i in nums if i != 0]
+        return nums+([0]*zeros)
